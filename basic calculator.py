bcd2194bb29e802eff48b1050149d27e6372418e
GordeOmkar05 <158061735+GordeOmkar05@users.noreply.github.com>
2024-08-06 18:11:55 +0530

    calculator.py

--- basic calculator.py
+++ basic calculator.py
@@ -1,5 +1,5 @@
 
-# This is Basic Calculator for CodSoft Internship
+
 
 def calculator():
     print("\n---------------WELCOME TO SIMPLE CALCULATOR---------------\n")
@@ -8,36 +8,33 @@ def calculator():
     
     while True:
         num1=float(input("Enter the input of first element: "))
-        if(num1=='.'):
-             num1=float(num1)
-        else:
-             num1=int(num1)
+        
         num2=float(input("Enter the input of second element: "))
-        if(num2=='.'):
-             num2=float(num2)
-        else:
-             num2=int(num2)
+        
         print("Please enter operation to be done:")
-        print("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Mod Division\n")
+        print(int("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Mod Division\n"))
         opr=input()
         match opr:
              case "1":
                  result= num1 + num2
+                 print(f"Result of Addition of {num1} + {num2} is:{result}")
            
              case "2":
                   result= num1 - num2
+                  print(f"Result of Substraction of {num1} - {num2} is:{result}")
              case "3":
                   result= num1 * num2
+                  print(f"Result of Multiplication of {num1} * {num2} is:{result}")
              case "4":
                   result= num1 / num2
+                  print(f"Result of Division of {num1} / {num2} is:{result}")
              case "5":
                   result= num1 % num2
+                  print(f"Result of Mod Division of {num1} * {num2} is:{result}")
              case _:
                   print("Please give valid inputs!!")
                   return
-        if isinstance(result, float) and result.is_integer():
-             result = int(result)
-        print(f"Result: {result}")
+        
                   
         user_response=input("Do you want to continue (yes/no):")
         if(user_response!="yes"):
